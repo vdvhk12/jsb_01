29e7feb7542169474cb9b7061a3c6088c3477b43
vdvhk12 <vdvhk12@gmail.com>
2024-12-10 10:56:28 +0900

    2-05-1 리포지터리 생성하기

---
+++ src/main/java/org/example/jsb_01/repository/QuestionRepository.java
@@ -0,0 +1,8 @@
+package org.example.jsb_01.repository;
+
+import org.example.jsb_01.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface QuestionRepository extends JpaRepository<Question, Integer> {
+
+}
