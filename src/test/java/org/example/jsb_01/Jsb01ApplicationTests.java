9e1092b312c09462e4915ddc5931ce9ec50f00e6
vdvhk12 <vdvhk12@gmail.com>
2024-12-10 09:49:50 +0900

    Initial commit

---
+++ src/test/java/org/example/jsb_01/Jsb01ApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jsb_01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Jsb01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
