356a2e25863089bd6045002aa2ccfa3245393960
vdvhk12 <vdvhk12@gmail.com>
2024-12-10 20:22:05 +0900

    2-11 URL 프리픽스 알아두기

--- src/main/java/org/example/jsb_01/question/QuestionController.java
+++ src/main/java/org/example/jsb_01/question/QuestionController.java
@@ -6,21 +6,23 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
+@RequestMapping("/question")
 public class QuestionController {
 
     private final QuestionService questionService;
 
-    @GetMapping("/question/list")
+    @GetMapping("/list")
     public String list(Model model) {
         List<QuestionDto> questions = questionService.getList();
         model.addAttribute("questions", questions);
         return "question_list";
     }
 
-    @GetMapping("/question/detail/{id}")
+    @GetMapping("/detail/{id}")
     public String detail(@PathVariable("id") Long id, Model model) {
         System.out.println("id = " + id);
         QuestionDto question = questionService.getQuestion(id);
