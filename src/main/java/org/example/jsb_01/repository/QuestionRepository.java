eb8b888d65d5a747030e840a0d2ffd5c3ad0851b
vdvhk12 <vdvhk12@gmail.com>
2024-12-10 11:40:54 +0900

    2-05-2 질문 데이터 생성 & 테스트

--- src/main/java/org/example/jsb_01/repository/QuestionRepository.java
+++ src/main/java/org/example/jsb_01/repository/QuestionRepository.java
@@ -1,8 +1,10 @@
 package org.example.jsb_01.repository;
 
+import java.util.Optional;
 import org.example.jsb_01.Question;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.lang.NonNull;
 
-public interface QuestionRepository extends JpaRepository<Question, Integer> {
-
+public interface QuestionRepository extends JpaRepository<Question, Long> {
+    Optional<Question> findById(@NonNull Long id);
 }
--- src/test/java/org/example/jsb_01/Jsb01ApplicationTests.java
+++ src/test/java/org/example/jsb_01/Jsb01ApplicationTests.java
@@ -1,13 +1,46 @@
 package org.example.jsb_01;
 
+import static org.assertj.core.api.Assertions.*;
+
+import jakarta.transaction.Transactional;
+import java.time.LocalDateTime;
+import org.example.jsb_01.repository.QuestionRepository;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class Jsb01ApplicationTests {
 
+	@Autowired
+	private QuestionRepository questionRepository;
+
 	@Test
-	void contextLoads() {
+	@Transactional
+	@DisplayName("Question 생성 테스트")
+	void testJpa() {
+	    //given
+		Question q1 = new Question();
+		q1.setSubject("sbb가 무엇인가요?");
+		q1.setContent("sbb에 대해서 알고 싶습니다.");
+		q1.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q1);
+
+		Question q2 = new Question();
+		q2.setSubject("스프링부트 모델 질문입니다.");
+		q2.setContent("id는 자동으로 생성되나요?");
+		q2.setCreateDate(LocalDateTime.now());
+		this.questionRepository.save(q2);
+
+		//when
+		Question question1 = questionRepository.findById(q1.getId()).get();
+		Question question2 = questionRepository.findById(q2.getId()).get();
+
+		//then
+		assertThat(q1.getContent()).isEqualTo(question1.getContent());
+		assertThat(q2.getContent()).isEqualTo(question2.getContent());
 	}
+
 
 }
